bdd4d7049c83eabcf3395563039ebe7eae4a5594
Dipen Sekhsaria <dipen0510@gmail.com>
2017-02-04 12:57:18 +0530

    - Wise Men app store 1.3 release

--- Voujon/AppDelegate.m
+++ Voujon/AppDelegate.m
@@ -36,7 +36,7 @@
     
     [Stripe setDefaultPublishableKey:@"pk_test_3Uvqk3cfNhYfDMvrqXt7rCEF"];
     
-    [OneSignal initWithLaunchOptions:launchOptions appId:@"03edc059-45f4-44ab-a8a2-7d47a330be26"];
+    [OneSignal initWithLaunchOptions:launchOptions appId:@"c59cbebb-5a0c-43f1-915f-31756965a48e"];
     
     return YES;
 }
