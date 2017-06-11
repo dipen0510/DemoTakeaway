bce4ef938dd153b01d83d01f1ec55290b9300851
DipenSekhsaria-Infy <dipen_sekhsaria@infosys.com>
2017-06-11 20:01:27 +0530

    - v1.0 App Store release

--- Voujon/AppDelegate.m
+++ Voujon/AppDelegate.m
@@ -36,7 +36,7 @@
     
     [Stripe setDefaultPublishableKey:@"pk_test_3Uvqk3cfNhYfDMvrqXt7rCEF"];
     
-    [OneSignal initWithLaunchOptions:launchOptions appId:@"c59cbebb-5a0c-43f1-915f-31756965a48e"];
+    [OneSignal initWithLaunchOptions:launchOptions appId:@"4aeba684-2fcb-4e5f-b93f-95ea7b2b1334"];
     
     return YES;
 }
